10e8996695216ecd5d66fd3e8edc874c283c66e0
= <harmostparmod@gmail.com>
2024-05-27 13:45:29 +0530

    added : models

---
+++ models/OTP.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  otp: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    expires: 5 * 60,
+  },
+});
+
+// send mail
+async function sendVerificationEmail(email, otp) {
+  try {
+    const mailResponse = await mailSender(
+      email,
+      "Verification Email from Learnify",
+      otp
+    );
+    console.log("Email sent successfully: ", mailResponse);
+  } catch (error) {
+    console.log("Error occurred while sending email: ", error);
+    throw error;
+  }
+}
+
+otpSchema.pre("save", async function (next) {
+  console.log("New document saved to database");
+  await sendVerificationEmail(this.email, this.otp);
+
+  next();
+});
+
+module.exports = mongoose.model("OTP", otpSchema);
